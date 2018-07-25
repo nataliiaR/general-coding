600359dd1de46378f5e3b8379d9ef7597591ed33
Nataliia Ryzha <nataliiaryzha@Nataliias-MacBook-Air.local>
2018-07-25 16:05:27 -0700

    finesse

--- src/main/java/FindMedianaReadFile.java
+++ src/main/java/FindMedianaReadFile.java
@@ -8,25 +8,34 @@ import java.util.*;
 
 
 public class FindMedianaReadFile {
+
     public static void main (String [] args){
 
         FindMedianaReadFile file = new FindMedianaReadFile();
         String path = "person_data_file";
-        file.medianValues(path);
-    }
+        HashMap<String,Integer> hash_map = file.medianValues(path);
+        System.out.println("Contents of file:");
+        Iterator it = hash_map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.println("City " + pair.getKey() + " median age value " + pair.getValue());
+            it.remove();
 
+        }
+
+    }
 
     public HashMap <String, Integer> medianValues (String path){
         HashMap<String, Integer> medianValuesMap = new HashMap<>();
+        HashMap<String, ArrayList<String>> hash_map = new HashMap<>();
         try {
             File file = new File(path);
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            StringBuffer stringBuffer = new StringBuffer();
             String line;
-            HashMap<String, ArrayList<String>> hash_map = new HashMap<>();
 
             while ((line = bufferedReader.readLine()) != null) {
+
                 ArrayList<String> current = new ArrayList<>();
 
                 if (hash_map.get(line.split(",")[1]) == null){
@@ -34,7 +43,7 @@ public class FindMedianaReadFile {
                     current.add(line.split(",")[2]);
 
                     hash_map.put(line.split(",")[1],current);
-                    
+
                 }
                 else {
                     current = hash_map.get(line.split(",")[1]);
@@ -44,12 +53,10 @@ public class FindMedianaReadFile {
             }
 
             fileReader.close();
-            System.out.println("Contents of file:");
             Iterator it = hash_map.entrySet().iterator();
             while (it.hasNext()) {
                 Map.Entry pair = (Map.Entry)it.next();
-                System.out.println(pair.getKey() + " = " + pair.getValue());
-                System.out.println(pair.getKey() + " median = " + median((ArrayList<String>) pair.getValue()));
+                medianValuesMap.put((String) pair.getKey(),median((ArrayList<String>) pair.getValue()));
                 it.remove();
 
         }
@@ -62,6 +69,7 @@ public class FindMedianaReadFile {
         return medianValuesMap;
     }
 
+
     public Integer median (ArrayList<String> arraylist){
         Integer medianValue = 0;
         Collections.sort(arraylist);
@@ -71,27 +79,5 @@ public class FindMedianaReadFile {
 
     }
 
-    /*public HashMap<String, ArrayList<String>> writeToHashMap (String line){
-        ArrayList<String> current = new ArrayList<>();
-        HashMap<String, ArrayList<String>> hash_map = new HashMap<>();
-
-        if (hash_map.get(line.split(",")[1]) == null){
-
-            current.add(line.split(",")[2]);
-
-            hash_map.put(line.split(",")[1],current);
-
-            System.out.println("for this key " + line.split(",")[1] + " we have this value " + hash_map.get(line.split(",")[1]));
-        }
-        else {
-            current = hash_map.get(line.split(",")[1]);
-            current.add(line.split(",")[2]);
-            hash_map.put(line.split(",")[1],current);
-
-
-        }
-
-        return hash_map;
-    }*/
 
 }
